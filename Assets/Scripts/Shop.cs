5731bb4a4f92b5795f11147d6ac6ae5bdbacacf1
Nicholas Chin <98058589+NichChin@users.noreply.github.com>
2024-10-06 15:10:39 -0400

    added outline and changed prefab in levelmanager. lvl 1 working

--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -1,16 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Shop : MonoBehaviour
 {
     [SerializeField] Animator anim;
 
+    public static Shop main;
     private bool isMenuOpen = true;
+    private Outline[] outlines;
+    private void Awake()
+    {
+        main = this;
+        outlines = GetComponentsInChildren<Outline>();
+    }
 
     public void ToggleMenu()
-    {   
+    {
         isMenuOpen = !isMenuOpen;
         anim.SetBool("MenuOpen", isMenuOpen);
+    }
+    public void SetSelectedTower(int selectedTower)
+    {
+        switch (selectedTower)
+        {
+            case 0:
+                outlines[0].enabled = true;
+                outlines[1].enabled = false;
+                break;
+            case 1:
+                outlines[1].enabled = true;
+                outlines[0].enabled = false;
+                break;
+        }
     }
 }
