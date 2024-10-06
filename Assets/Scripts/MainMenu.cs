95e4f6978aab466303b7fa62c6cc3d83deb7d852
grakp <grace.zhao325@gmail.com>
2024-10-06 11:06:02 -0400

    dynamic level loading

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -10,21 +10,10 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene("LevelSelect"); // change to Level1, then further levels index +1
 
     }
-    
-    // could probably dynamically choose level based on button pressed but...
-    public void Level1()
-    {
-        SceneManager.LoadScene("Level1");
-    }
 
-    public void Level2()
+    public void LoadLevel(string levelName)
     {
-        SceneManager.LoadScene("Level2");
-    }
-
-    public void Level3()
-    {
-        SceneManager.LoadScene("Level3");
+        SceneManager.LoadScene(levelName);
     }
 
     public void QuitGame()
