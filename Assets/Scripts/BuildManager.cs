b2a92e7ae9f56d56d0f05b95547a06f079e3d85f
Nicholas Chin <98058589+NichChin@users.noreply.github.com>
2024-10-06 14:35:37 -0400

    Revert "added outline for selected shop item and fixed prefabs for button"
    This reverts commit 69760964a7790fd6b11495ce290a987ca969060a.

--- Assets/Scripts/BuildManager.cs
+++ Assets/Scripts/BuildManager.cs
@@ -28,6 +28,5 @@ public class BuildManager : MonoBehaviour
     {
         print(_selectedTower);
         selectedTower = _selectedTower;
-        Shop.main.SetSelectedTower(selectedTower);
     }
 }
